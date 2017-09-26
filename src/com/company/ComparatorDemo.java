07ac6e74b80272b88232c1f8623c68e21ba14b26
Savannah Lowder <savannahlowder@Savannahs-MacBook-Air.local>
2017-09-26 16:24:23 -0400

    committing mini project

---
+++ src/com/company/ComparatorDemo.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static com.sun.tools.doclint.Entity.lambda;
+import static jdk.nashorn.internal.objects.Global.print;
+
+/**
+ * Created by gilpratte on 7/26/17.
+ */
+public class ComparatorDemo {
+
+    public static void main(String[] args) {
+        List<Address> addresses = new ArrayList<>();
+        addresses.add(new Address("123 Main St.", "Austin", "TX", "78787"));
+        addresses.add(new Address("43 High St.", "Columbus", "OH", "43034"));
+        addresses.add(new Address("678 Leon Rd.", "Tallahassee", "FL", "32307"));
+        addresses.add(new Address("8 Deep Sea Way", "Boca Raton", "FL", "33433"));
+
+        //Feel free to add in more addresses if you would like
+
+        System.out.println("Before sorting");
+        print(addresses);
+
+        // !! - Write an anonymous class to sort by state (alphabetically)
+        Collections.sort(addresses, new Comparator<Address> () {
+            @Override
+            public int compare(Address one, Address two) {
+                return one.getState().compareTo(two.getState());
+            }/* anonymous class here */
+        });
+
+        System.out.println("\nAfter sorting by state");
+        print(addresses);
+
+        // !! - Write a lambda to sort by city alphabetically
+        Collections.sort(addresses, (one, two) -> one.getCity().compareTo(two.getCity()));/* lambda here */
+
+        System.out.println("\nAfter sorting by city");
+        print(addresses);
+    }
+
+    private static void print(List<Address> addresses) {
+        for(Address address : addresses) {
+            System.out.println(address);
+        }
+    }
+}
